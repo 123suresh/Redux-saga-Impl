6dd0a072e62cf35417b3c1e452920a0db4ee69bc
123suresh <50450847+123suresh@users.noreply.github.com>
2023-06-09 14:56:14 +0545

    permanent store for redux storage by persist

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component, Fragment } from 'react';
 import { Provider } from 'react-redux';
 import MainRoutes from './router/MainRouter';
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 import configureStore from './store';
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Fragment>
-                    <MainRoutes />
-                </Fragment>
-            </Provider>
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
+          </Provider>
         );
     }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,34 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import rootSaga from '../sagas';
-
 import rootReducer from '../reducers';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const configureStore = () => {
-    const sagaMiddleware = createSagaMiddleware();
-    const store = createStore(
-        rootReducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__
-            ? compose(
-                  applyMiddleware(sagaMiddleware),
-                  window.__REDUX_DEVTOOLS_EXTENSION__(),
-              )
-            : applyMiddleware(sagaMiddleware),
-    );
-    sagaMiddleware.run(rootSaga);
-    return store;
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? compose(
+          applyMiddleware(sagaMiddleware),
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+      : applyMiddleware(sagaMiddleware)
+  );
+
+  const persistor = persistStore(store);
+  sagaMiddleware.run(rootSaga);
+
+  return { store, persistor };
 };
 
 export default configureStore;
